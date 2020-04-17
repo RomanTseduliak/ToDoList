01f400c5fea80fbee43012bf1606993308225cea
Roman Tseduliak <51488835+RomanTseduliak@users.noreply.github.com>
2020-04-17 22:45:04 +0300

    Add files via upload

---
+++ build/precache-manifest.bf98c26d5bf26e9463d303aa3eb8d110.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f69e249be10bb27a1034f2f24c941a44",
+    "url": "/-/index.html"
+  },
+  {
+    "revision": "7b640ff889d69b1dda6e",
+    "url": "/-/static/css/main.cfe6b972.chunk.css"
+  },
+  {
+    "revision": "cc0b32ba4b8595bffd83",
+    "url": "/-/static/js/2.bb16d8ff.chunk.js"
+  },
+  {
+    "revision": "7b640ff889d69b1dda6e",
+    "url": "/-/static/js/main.a2cf07ee.chunk.js"
+  },
+  {
+    "revision": "633d3e6657e472657000",
+    "url": "/-/static/js/runtime~main.17514c12.js"
+  }
+]);
