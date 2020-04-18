43dd40ceafffe9c87ae91e64abb1b329b436f622
Roman Tseduliak <Alligarch@gmail.com>
2020-04-18 09:36:01 +0000

    Updates

---
+++ precache-manifest.0eaa38910fa2c776fa4c8c4fd731d502.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8a4b29e77681bf25fa66d9bac0c6b00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f3a7d27685cb00f19b3",
+    "url": "/static/css/main.cfe6b972.chunk.css"
+  },
+  {
+    "revision": "cc0b32ba4b8595bffd83",
+    "url": "/static/js/2.bb16d8ff.chunk.js"
+  },
+  {
+    "revision": "5f3a7d27685cb00f19b3",
+    "url": "/static/js/main.45f98ff6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
